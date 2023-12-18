8c927e186994b7396adbe792d307ebe1dec16cba
Raquel Guzansky <raquelguzansky@MacBook-Air-de-Raquel.local>
2023-12-18 20:25:46 -0300

    index: inclusao de react native nas opcoes, abrindo normal! verificar configuracao do snacs no medicamix para ver o que falta para o app aparecer

---
+++ assets/js/snacs.js
@@ -0,0 +1,8 @@
+<div
+    data-snack-id="@react-navigation/basic-scrollview-tab-v3"
+    data-snack-platform="web"
+    data-snack-preview="true"
+    data-snack-theme="light"
+    data-snack-loading="lazy"
+    style="overflow:hidden;background:#fafafa;border:1px solid rgba(0,0,0,.08);border-radius:4px;height:505px;width:100%">
+</div>
